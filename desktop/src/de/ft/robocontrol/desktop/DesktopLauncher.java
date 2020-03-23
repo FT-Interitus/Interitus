c1927f0cfb19fd95a573dad7df509a04ec52d645
Coder246 <tim-arnold@outlook.de>
2020-03-23 17:27:59 +0100

    Add Java libgdx files

---
+++ desktop/src/de/ft/robocontrol/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.ft.robocontrol.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.ft.robocontrol.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
