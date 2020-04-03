7d003608ca9a7a07c16423b36be03e839fbda990
Felixpc <46627355+Felixpc@users.noreply.github.com>
2020-04-03 10:12:53 +0200

    fullscreen mode unter Ansicht in menuebar

--- desktop/src/de/ft/robocontrol/desktop/DesktopLauncher.java
+++ desktop/src/de/ft/robocontrol/desktop/DesktopLauncher.java
@@ -3,12 +3,14 @@ package de.ft.robocontrol.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import de.ft.robocontrol.MainGame;
+import org.lwjgl.opengl.Display;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.height=800;
 		config.width=1300;
+		config.height=800;
 		config.vSyncEnabled=false;
 		new LwjglApplication(new MainGame(), config);
 	}
