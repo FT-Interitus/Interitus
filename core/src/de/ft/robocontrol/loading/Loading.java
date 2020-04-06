12698592478f21d1a67e7d55d8a1b0a0a27d7a27
Felixpc <46627355+Felixpc@users.noreply.github.com>
2020-04-06 15:21:46 +0200

    imports weg

--- core/src/de/ft/robocontrol/loading/Loading.java
+++ core/src/de/ft/robocontrol/loading/Loading.java
@@ -2,16 +2,11 @@ package de.ft.robocontrol.loading;
 
 import com.badlogic.gdx.*;
 
-import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 import de.ft.robocontrol.MainGame;
 import de.ft.robocontrol.Programm;
 import de.ft.robocontrol.Var;
 import de.ft.robocontrol.Welcome;
-import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.GLContext;
 
 
 
