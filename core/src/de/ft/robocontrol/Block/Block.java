ff70efde534e61c8a456e542a77eeab82df76f5d
Felixpc <46627355+Felixpc@users.noreply.github.com>
2020-04-06 15:18:30 +0200

    Revert "Merge branch 'master' of https://github.com/Felixpc/RoboControl"
    This reverts commit 1b448e8820e896b145bcb677982827765faab7fc, reversing
    changes made to 8ac385afb700acadb34376b8f922b061aab75c40.

--- core/src/de/ft/robocontrol/Block/Block.java
+++ core/src/de/ft/robocontrol/Block/Block.java
@@ -5,12 +5,12 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Frustum;
-import de.ft.robocontrol.loading.AssetLoader;
 import de.ft.robocontrol.MainGame;
 import de.ft.robocontrol.ThreadManager;
 import de.ft.robocontrol.data.user.changes.DataManager;
-import de.ft.robocontrol.loading.AssetLoader;
 import de.ft.robocontrol.utils.CheckKollision;
+
+import java.util.concurrent.ExecutionException;
 
 public class Block {
     public boolean seted = true;
@@ -262,24 +262,24 @@ public class Block {
 
 
         if (!this.blockupdate.toggle) {
-            batch.draw(AssetLoader.img_block, this.getX(), this.getY(), this.getW(), this.getH());
+            batch.draw(MainGame.img_block, this.getX(), this.getY(), this.getW(), this.getH());
         } else {
-            batch.draw(AssetLoader.img_block_mouseover, this.getX(), this.getY(), this.getW(), this.getH());
+            batch.draw(MainGame.img_mouseover, this.getX(), this.getY(), this.getW(), this.getH());
         }
 
         if (this.isMarked()) {
-            batch.draw(AssetLoader.img_marked, this.getX(), this.getY(), this.getW(), this.getH());
+            batch.draw(MainGame.img_marked, this.getX(), this.getY(), this.getW(), this.getH());
         }
         if (BlockVar.biggestblock == this) {
             if (this.isShowdupulicate_rechts()) {
                 batch.setColor(1, 1, 1, 0.5f);
-                batch.draw(AssetLoader.img_block, this.x_dup_rechts, this.y, this.getW(), this.getH());
+                batch.draw(MainGame.img_block, this.x_dup_rechts, this.y, this.getW(), this.getH());
                 batch.setColor(1, 1, 1, 1);
             }
 
             if (this.isShowdupulicate_links()) {
                 batch.setColor(1, 1, 1, 0.5f);
-                batch.draw(AssetLoader.img_block, this.x_dup_links, this.y, this.getW(), this.getH());
+                batch.draw(MainGame.img_block, this.x_dup_links, this.y, this.getW(), this.getH());
                 batch.setColor(1, 1, 1, 1);
             }
         }
--- desktop/src/de/ft/robocontrol/desktop/DesktopLauncher.java
+++ desktop/src/de/ft/robocontrol/desktop/DesktopLauncher.java
@@ -1,37 +1,18 @@
 package de.ft.robocontrol.desktop;
 
-import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplet;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.badlogic.gdx.backends.lwjgl.audio.OpenALAudio;
 import de.ft.robocontrol.MainGame;
-import de.ft.robocontrol.Programm;
 import de.ft.robocontrol.Var;
-import de.ft.robocontrol.data.user.LoadSave;
-import de.ft.robocontrol.loading.AssetLoader;
-import de.ft.robocontrol.loading.Loading;
-import org.lwjgl.LWJGLException;
-import org.lwjgl.openal.AL;
 import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.GLContext;
-
-
 
 public class DesktopLauncher {
-
 	public static void main (String[] arg) {
-
-
 
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width= Var.w;
 		config.height=Var.h;
 		config.vSyncEnabled=false;
-		new LwjglApplication(new Programm(), config);
-
-
-
+		new LwjglApplication(new MainGame(), config);
 	}
-
 }
