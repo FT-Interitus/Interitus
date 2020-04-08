8e55fb11564dd2ef4b3a863d39168c321e5924e8
Coder246 <tim-arnold@outlook.de>
2020-04-08 20:20:00 +0200

    NetworkScan angefangen

--- core/src/de/ft/robocontrol/utils/NetworkScan.java
+++ core/src/de/ft/robocontrol/utils/NetworkScan.java
@@ -4,28 +4,42 @@ import java.io.IOException;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 public class NetworkScan {
     public static void get() {
-        InetAddress byName = null;
-        try {
-            byName = InetAddress.getByName("192.168.2.2");
+        ArrayList<InetAddress> device = new ArrayList<>();
 
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-        System.out.println(byName);
         try {
-            System.out.println(byName.isReachable(1000));
+
             try(final DatagramSocket socket = new DatagramSocket()){
                 socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
                 String ip = socket.getLocalAddress().getHostAddress();
-             System.out.println(ip.split("\\.")[2]);
-                System.out.println(ip.split("\\.")[2]);
-                System.out.println(ip.split("\\.")[2]);  System.out.println(ip.split("\\.")[2]);
-                System.out.println(ip.split("\\.")[2]);  System.out.println(ip.split("\\.")[2]);  System.out.println(ip.split("\\.")[2]);
 
 
+
+
+               ip = ip.split("\\.")[2];
+
+               try {
+
+                   System.out.println("Found Raspberry at "+ InetAddress.getByName("raspberrypi").getHostAddress());
+               }catch (UnknownHostException e) {
+                  for(int i = 0;i<255;i++) {
+                     InetAddress testdevice =InetAddress.getByName("192.168."+ip+"."+i);
+
+                     if(testdevice.isReachable(35)) {
+                         device.add(testdevice);
+                     }
+
+                  }
+
+
+                  for(int i =0;i<device.size();i++) {
+
+                      System.out.println(device.get(i).getHostName());
+                  }
+               }
 
 
 
