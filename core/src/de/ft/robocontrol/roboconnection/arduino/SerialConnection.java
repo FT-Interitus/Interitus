23387f52e9200d58e46c54c4b07c38080e083ac7
Felixpc <46627355+Felixpc@users.noreply.github.com>
2020-04-08 20:18:20 +0200

    übersicht

--- core/src/de/ft/robocontrol/roboconnection/arduino/SerialConnection.java
+++ core/src/de/ft/robocontrol/roboconnection/arduino/SerialConnection.java
@@ -79,100 +79,146 @@ public static boolean isRunning=false;
     }
 
 
-public static void searchArduino() {
-    // determine which serial port to use
+    public static void searchArduino(){
+        SerialConnection.Authentifikation.searchArduino();
+    }
 
-    Thread search = new Thread() {
-        public void run() {
-            isRunning=true;
-            try {
-//TODO hier System.out's rausschmeisen
-                SerialPort ports[] = SerialPort.getCommPorts();
 
-                i = 1;
 
-                for (SerialPort port : ports) {
-                    i++;
 
-                    System.out.println("i     " + i);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static class Authentifikation {
+        public static void searchArduino() {
+            // determine which serial port to use
+
+            Thread search = new Thread() {
+                public void run() {
+                    isRunning=true;
                     try {
-                        SerialPort testport = ports[i-2];
+//TODO hier System.out's rausschmeisen
+                        SerialPort ports[] = SerialPort.getCommPorts();
 
-                        if (testport.openPort()) {
-                            System.out.println("Successfully opened the port."+testport.getSystemPortName());
-                            System.out.println("desprictiveportname:   " + testport.getDescriptivePortName());
-                        } else {
-                            System.out.println("Unable to open the port.");
+                        i = 1;
 
-                        }
-                        System.out.println("1");
+                        for (SerialPort port : ports) {
+                            i++;
 
-                        testport.setBaudRate(230400);
-                        System.out.println("2");
-                        testport.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 10, 0);
-                        System.out.println("3");
+                            System.out.println("i     " + i);
+                            try {
+                                SerialPort testport = ports[i-2];
 
-                         save = System.currentTimeMillis() + 5000;
-                      found = false;
-                      System.out.println("checkstart");
-                        while (System.currentTimeMillis() < save && found == false) {
-                            if (empfangen(testport) == 1234) {
-                                System.out.println("Arduino gefunden");
-                                found = true;
-                            }
-                        }
-                        System.out.println("checkend");
-                System.out.println(found);
-                        if (found == false) {
-                            testport.closePort();
-                            System.out.println("arduino nicht gefunden");
-                        }
-                        if (found == true) {
+                                if (testport.openPort()) {
+                                    System.out.println("Successfully opened the port."+testport.getSystemPortName());
+                                    System.out.println("desprictiveportname:   " + testport.getDescriptivePortName());
+                                } else {
+                                    System.out.println("Unable to open the port.");
 
-                            System.out.println("arduino gefunden");
+                                }
+                                System.out.println("1");
 
-                            if (Arduinos.size() == 0) {
-                                Arduinos.add(testport);
-                            } else {
-                                for (int e = 0; e < Arduinos.size(); e++) {
+                                testport.setBaudRate(230400);
+                                System.out.println("2");
+                                testport.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 10, 0);
+                                System.out.println("3");
 
-                                    if (Arduinos.get(e).getSystemPortName().equals(testport.getSystemPortName())) {
+                                save = System.currentTimeMillis() + 5000;
+                                found = false;
+                                System.out.println("checkstart");
+                                while (System.currentTimeMillis() < save && found == false) {
+                                    if (empfangen(testport) == 1234) {
+                                        System.out.println("Arduino gefunden");
+                                        found = true;
+                                    }
+                                }
+                                System.out.println("checkend");
+                                System.out.println(found);
+                                if (found == false) {
+                                    testport.closePort();
+                                    System.out.println("arduino nicht gefunden");
+                                }
+                                if (found == true) {
 
-                                    } else {
+                                    System.out.println("arduino gefunden");
+
+                                    if (Arduinos.size() == 0) {
                                         Arduinos.add(testport);
+                                    } else {
+                                        for (int e = 0; e < Arduinos.size(); e++) {
+
+                                            if (Arduinos.get(e).getSystemPortName().equals(testport.getSystemPortName())) {
+
+                                            } else {
+                                                Arduinos.add(testport);
+                                            }
+
+
+                                        }
                                     }
 
 
                                 }
+
+
+                                testport.closePort();
+
+                            } catch (ArrayIndexOutOfBoundsException e) {
+
+                                e.printStackTrace();
                             }
 
 
                         }
 
 
-                        testport.closePort();
-
-                    } catch (ArrayIndexOutOfBoundsException e) {
-
-                        e.printStackTrace();
+                        if(Step3.selectportlist!=null) {
+                            Step3.selectportlist.setItems(SerialConnection.getPortNames());
+                        }
+                    }catch (Exception e) {
+                        e.printStackTrace(); //for debug to find errors
                     }
 
-
+                    isRunning=false;
                 }
-
-
-            if(Step3.selectportlist!=null) {
-                Step3.selectportlist.setItems(SerialConnection.getPortNames());
+            };
+            if(!isRunning) {
+                search.start();
             }
-            }catch (Exception e) {
-                e.printStackTrace(); //for debug to find errors
-            }
-
-            isRunning=false;
         }
-    };
-if(!isRunning) {
-    search.start();
-}
-}
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
