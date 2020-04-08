d657e73e4fc0cc66388a212ddab6ff25e6eb6ecb
Coder246 <tim-arnold@outlook.de>
2020-04-08 13:09:49 +0200

    Programm lauf zeit analyse

--- core/src/de/ft/robocontrol/Programm.java
+++ core/src/de/ft/robocontrol/Programm.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.ScreenAdapter;
 import de.ft.robocontrol.data.programm.Data;
+import de.ft.robocontrol.data.user.experience.Counter;
 import de.ft.robocontrol.loading.Loading;
 
 public class Programm extends Game {
@@ -17,6 +18,8 @@ public class Programm extends Game {
     }
     @Override
     public void create() {
+
+        Counter.init();
         setScreen(new Loading());
     }
 
