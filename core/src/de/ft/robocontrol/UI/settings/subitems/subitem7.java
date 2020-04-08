5a60bfa8d270f25dc714fcf855166c29f972b2c2
Coder246 <tim-arnold@outlook.de>
2020-04-08 15:04:01 +0200

    level 2

--- core/src/de/ft/robocontrol/UI/settings/subitems/subitem7.java
+++ core/src/de/ft/robocontrol/UI/settings/subitems/subitem7.java
@@ -6,6 +6,9 @@ import com.kotcrab.vis.ui.widget.VisTable;
 
 public class subitem7 {
     public static void add(VisTable builder) {
-        builder.add(new VisLabel("Empty")).expandX().fillY();
+
+        builder.add(new VisLabel("Personalisyte Tipps")).expandX().fillY();
+
+        //TODO hier toggle für tipps
     }
 }
