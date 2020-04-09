30b81b309013a517b58582b80f4cf8cc46fbcb09
Coder246 <tim-arnold@outlook.de>
2020-04-09 17:29:43 +0200

    ssh klasse angefangen

--- core/src/de/ft/robocontrol/roboconnection/raspberrypi/SSHConnection.java
+++ core/src/de/ft/robocontrol/roboconnection/raspberrypi/SSHConnection.java
@@ -1,10 +1,50 @@
 package de.ft.robocontrol.roboconnection.raspberrypi;
 
-import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.*;
 
 public class SSHConnection {
-    public void connect() {
+   private static JSch testconnection = new JSch();
+   private static Session testconnectionsession;
+    public static void connect() {
         JSch jSch = new JSch();
-        String host=null;
+        String host="192.168.2.117";
+        String user = "pi";
+        String password = "Pi-Server";
+
+        try {
+            Session session = jSch.getSession(user,host,22);
+            session.setPassword(password);
+            session.setConfig("StrictHostKeyChecking", "no");
+
+            session.connect(30000);
+
+            Channel channel=session.openChannel("exec");
+
+            ((ChannelExec) channel).setCommand("echo Hi Pi> test");
+
+            channel.connect(3*1000);
+            channel.disconnect();
+            ((ChannelExec) channel).setCommand("echo Hi Pi> test3");
+            channel.connect(3000);
+
+
+        } catch (JSchException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static boolean checkconnection(String ipaddress,String username,String password) {
+        try {
+           testconnectionsession = testconnection.getSession(username,ipaddress,22); //TODO port auswählbar
+           testconnectionsession.setPassword(password);
+           testconnectionsession.connect();
+           testconnectionsession.disconnect();
+           return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
