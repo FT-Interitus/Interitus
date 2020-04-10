363f2da467251348ad650d1374f563788037ca12
Coder246 <tim-arnold@outlook.de>
2020-04-10 12:08:08 +0200

    timeout für langsmae geräte

--- core/src/de/ft/robocontrol/utils/NetworkScan.java
+++ core/src/de/ft/robocontrol/utils/NetworkScan.java
@@ -33,7 +33,7 @@ public class NetworkScan {
                   for(int i = 0;i<255;i++) {
                      InetAddress testdevice =InetAddress.getByName("192.168."+ip+"."+i);
 
-                     if(testdevice.isReachable(40)) { //TODO bester wert ermittlen
+                     if(testdevice.isReachable(45)) { //TODO bester wert ermittlen
                          device.add(testdevice);
                      }
 
