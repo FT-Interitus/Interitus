d068034f15f586d3c022a9d0b12763ed037cafc1
Coder246 <tim-arnold@outlook.de>
2020-04-29 17:45:58 +0200

    Block Variable wurde in event nicht richtig gesetzt

--- core/src/de/ft/interitus/events/block/BlockDeleteEvent.java
+++ core/src/de/ft/interitus/events/block/BlockDeleteEvent.java
@@ -14,6 +14,7 @@ public class BlockDeleteEvent extends EventObject {
      */
     public BlockDeleteEvent(Object source, Block block) {
         super(source);
+        this.block = block;
     }
 
     public Block getBlock() {
