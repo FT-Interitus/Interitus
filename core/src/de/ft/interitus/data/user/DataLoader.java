2de0822ed35ac92dbf0b1e6489875f3fb9023ffa
Coder246 <tim-arnold@outlook.de>
2020-06-08 19:24:40 +0200

    Block verbindungen laden

--- core/src/de/ft/interitus/data/user/DataLoader.java
+++ core/src/de/ft/interitus/data/user/DataLoader.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.files.FileHandle;
 import de.ft.interitus.Block.BlockVar;
 import de.ft.interitus.Block.SaveBlock;
 import de.ft.interitus.Var;
+import de.ft.interitus.data.programm.Data;
 import de.ft.interitus.projecttypes.device.BlockTypes.Ev3.Wait;
 import de.ft.interitus.utils.ClearActOpenProgramm;
 import org.json.JSONObject;
@@ -22,14 +23,24 @@ public class DataLoader {
 
 
                 try {
-                    FileInputStream fis = new FileInputStream(handle.file());
+                    Zip.unzip(handle.file().getAbsolutePath(), Data.tempfolder.getAbsolutePath());
+                    FileHandle file = new FileHandle(Data.tempfolder.getAbsolutePath()+"/"+"Blocks.itid");
+                    FileInputStream fis = new FileInputStream(file.file());
                     ObjectInputStream ois = new ObjectInputStream(fis);
 
-                ArrayList<SaveBlock> readedblocks =    ((ArrayList<SaveBlock>) ois.readObject());
+                ArrayList<SaveBlock> readedblocks = ((ArrayList<SaveBlock>) ois.readObject());
 
                 for(int i=0;i<readedblocks.size();i++) {
                     BlockVar.blocks.add(Var.actProjekt.getBlockGenerator().generateBlock(i,readedblocks.get(i).getX(),readedblocks.get(i).getY(),150,70,new Wait(),Var.actProjekt.getBlockUpdateGenerator(),Var.actProjekt.getBlocktoSaveGenerator()));
                 }
+
+                for(int i=0;i<readedblocks.size();i++) {
+
+                    BlockVar.blocks.get(i).setRight(readedblocks.get(i).getIndex_rechts());
+                    BlockVar.blocks.get(i).setLeft(readedblocks.get(i).getIndex_links());
+                }
+
+
 
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
@@ -38,6 +49,10 @@ public class DataLoader {
                 } catch (ClassNotFoundException e) {
                     e.printStackTrace();
                 }
+
+                File tempblockfile = new File(Data.tempfolder + "/" + "Blocks.itid");
+                tempblockfile.delete();
+
 
 
                 Var.isloading = false;
