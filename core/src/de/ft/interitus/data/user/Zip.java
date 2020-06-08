2f3ef70b2b667bd39da4ded3ba15ae7985a4630c
Felixpc <46627355+Felixpc@users.noreply.github.com>
2020-06-08 10:51:51 +0200

    Update Zip.java

--- core/src/de/ft/interitus/data/user/Zip.java
+++ core/src/de/ft/interitus/data/user/Zip.java
@@ -2,6 +2,7 @@ package de.ft.interitus.data.user;
 
 import java.io.*;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
@@ -45,4 +46,43 @@ public class Zip {
         zipOut.close();
         fos.close();
     }
+
+
+
+
+
+
+
+    public static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
+        File destFile = new File(destinationDir, zipEntry.getName());
+
+        String destDirPath = destinationDir.getCanonicalPath();
+        String destFilePath = destFile.getCanonicalPath();
+
+        if (!destFilePath.startsWith(destDirPath + File.separator)) {
+            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+        }
+
+        return destFile;
+    }
+    public static void unzip(String fileZip, String destDirstr) throws IOException {
+        File destDir = new File(destDirstr);
+        byte[] buffer = new byte[1024];
+        ZipInputStream zis = new ZipInputStream(new FileInputStream(fileZip));
+        ZipEntry zipEntry = zis.getNextEntry();
+        while (zipEntry != null) {
+            File newFile = newFile(destDir, zipEntry);
+            FileOutputStream fos = new FileOutputStream(newFile);
+            int len;
+            while ((len = zis.read(buffer)) > 0) {
+                fos.write(buffer, 0, len);
+            }
+            fos.close();
+            zipEntry = zis.getNextEntry();
+        }
+        zis.closeEntry();
+        zis.close();
+    }
+
+
 }
