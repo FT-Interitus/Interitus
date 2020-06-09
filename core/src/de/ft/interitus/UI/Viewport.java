170fdd532baf77e779b74f84d008d9f3d52744ab
Coder246 <tim-arnold@outlook.de>
2020-06-09 13:15:59 +0200

    bewegen der camera mit den Pfeiltasten

--- core/src/de/ft/interitus/UI/Viewport.java
+++ core/src/de/ft/interitus/UI/Viewport.java
@@ -13,7 +13,18 @@ import de.ft.interitus.UI.inputfields.check.InputManager;
 import static com.badlogic.gdx.Gdx.input;
 
 public class Viewport {
+private static boolean run_left = false;
+private static boolean run_right = false;
+private static boolean run_up = false;
+private static boolean run_down = false;
+private static long time_pressed_left = 0;
+private static long time_pressed_right = 0;
+private static long time_pressed_up = 0;
+private static long time_pressed_down = 0;
 
+
+public static long movedelay = 400;
+    public static long firstmovedelay = 600;
     public static void init()   {
         InputManager.addProcessor(new InputAdapter(){
 
@@ -64,21 +75,70 @@ public class Viewport {
     }
 
     public static void update() {
-        if (input.isKeyJustPressed(Input.Keys.LEFT)) {
-            ProgrammingSpace.cam.position.set(ProgrammingSpace.cam.position.x -= 20, ProgrammingSpace.cam.position.y, 0);
+
+        if (input.isKeyPressed(Input.Keys.LEFT)) {
+            if(!run_left) {
+                time_pressed_left = System.currentTimeMillis();
+                run_left = true;
+                ProgrammingSpace.cam.position.set(ProgrammingSpace.cam.position.x -= 20, ProgrammingSpace.cam.position.y, 0);
+            }else{
+                if(System.currentTimeMillis()-time_pressed_left>firstmovedelay) {
+                    time_pressed_left = System.currentTimeMillis()-firstmovedelay-movedelay;
+                    ProgrammingSpace.cam.position.set(ProgrammingSpace.cam.position.x -= 20, ProgrammingSpace.cam.position.y, 0);
+                }
+            }
+        }else{
+            run_left = false;
         }
 
-        if (input.isKeyJustPressed(Input.Keys.RIGHT)) {
-            ProgrammingSpace.cam.position.set(ProgrammingSpace.cam.position.x += 20, ProgrammingSpace.cam.position.y, 0);
+
+        if (input.isKeyPressed(Input.Keys.RIGHT)) {
+            if(!run_right) {
+                time_pressed_right = System.currentTimeMillis();
+                run_right = true;
+                ProgrammingSpace.cam.position.set(ProgrammingSpace.cam.position.x += 20, ProgrammingSpace.cam.position.y, 0);
+            }else{
+                if(System.currentTimeMillis()-time_pressed_right>firstmovedelay) {
+                    time_pressed_right = System.currentTimeMillis()-firstmovedelay-movedelay;
+                    ProgrammingSpace.cam.position.set(ProgrammingSpace.cam.position.x += 20, ProgrammingSpace.cam.position.y, 0);
+                }
+            }
+        }else{
+            run_right = false;
         }
 
-        if (input.isKeyJustPressed(Input.Keys.UP)) {
-            ProgrammingSpace.cam.position.set(ProgrammingSpace.cam.position.x,ProgrammingSpace.cam.position.y += 20, 0);
+
+        if (input.isKeyPressed(Input.Keys.UP)) {
+            if(!run_up) {
+                time_pressed_up = System.currentTimeMillis();
+                run_up = true;
+                ProgrammingSpace.cam.position.set(ProgrammingSpace.cam.position.x, ProgrammingSpace.cam.position.y += 20, 0);
+            }else{
+                if(System.currentTimeMillis()-time_pressed_up>firstmovedelay) {
+                    time_pressed_up = System.currentTimeMillis()-firstmovedelay-movedelay;
+                    ProgrammingSpace.cam.position.set(ProgrammingSpace.cam.position.x, ProgrammingSpace.cam.position.y += 20, 0);
+                }
+            }
+        }else{
+            run_up = false;
         }
 
-        if (input.isKeyJustPressed(Input.Keys.DOWN)) {
-            ProgrammingSpace.cam.position.set(ProgrammingSpace.cam.position.x, ProgrammingSpace.cam.position.y -= 20, 0);
+
+        if (input.isKeyPressed(Input.Keys.DOWN)) {
+            if(!run_down) {
+                time_pressed_down = System.currentTimeMillis();
+                run_down = true;
+                ProgrammingSpace.cam.position.set(ProgrammingSpace.cam.position.x, ProgrammingSpace.cam.position.y -= 20, 0);
+            }else{
+                if(System.currentTimeMillis()-time_pressed_down>firstmovedelay) {
+                    time_pressed_down = System.currentTimeMillis()-firstmovedelay-movedelay;
+                    ProgrammingSpace.cam.position.set(ProgrammingSpace.cam.position.x, ProgrammingSpace.cam.position.y -= 20, 0);
+                }
+            }
+        }else{
+            run_down = false;
         }
+
 
         if(input.isKeyJustPressed(Input.Keys.PLUS)) {
             ProgrammingSpace.cam.zoom= ProgrammingSpace.cam.zoom-0.2f;
