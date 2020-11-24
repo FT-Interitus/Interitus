c8b9199bddbcc87801d011c61cb807f7b63b4e47
Coder246 <tim-arnold@outlook.de>
2020-11-24 18:36:43 +0100

    You can now install Plugin via Interitus

--- core/src/de/ft/interitus/plugin/store/StorePluginInstallManager.java
+++ core/src/de/ft/interitus/plugin/store/StorePluginInstallManager.java
@@ -9,6 +9,7 @@ package de.ft.interitus.plugin.store;
 import de.ft.interitus.Program;
 import de.ft.interitus.datamanager.programmdata.Data;
 import de.ft.interitus.utils.DownloadFile;
+import de.ft.interitus.utils.Restart;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,19 +32,22 @@ public class StorePluginInstallManager {
 
     @CrossOrigin() //TODO define Origins
     @RequestMapping(value = "/install")
-    public String installer(@RequestParam String url,@RequestParam String JsonInformation) throws IOException {
+    public String installer(@RequestParam String url,@RequestParam String jsoninformation) throws IOException {
 
         Program.logger.config(url);
 
-        JSONObject jsonObject = new JSONObject(JsonInformation);
-
+        JSONObject jsonObject = new JSONObject(jsoninformation);
+        Program.logger.config(jsonObject.getString("name"));
         //TODO check
         //installRequest(jsonObject.getString("name"),jsonObject.getString("version"),jsonObject.getString("description"),jsonObject.getString("author"),jsonObject.getString("detailed_description"));
-
+        Program.logger.config("path generatiing...");
         File newplugin = new File(Data.folder.getAbsolutePath()+"/plugins/"+jsonObject.getString("name")+".itpl");
+        Program.logger.config("downloading...");
         FileOutputStream fileOutputStream = new FileOutputStream(newplugin);
+        Program.logger.config("starting...");
         fileOutputStream.write(DownloadFile.downloadBytes(url));
-
+        Program.logger.config("restarting...");
+        Restart.restart();
             return "";
 
     }
