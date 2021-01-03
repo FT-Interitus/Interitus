5a163404ea5974a11fb88e86c4d517ae94b842de
Coder246 <tim-arnold@outlook.de>
2021-01-03 20:08:13 +0100

    Added TextLabel fixed width

--- core/src/de/ft/interitus/UI/UIContents/ZoomUi/ZoomUI.java
+++ core/src/de/ft/interitus/UI/UIContents/ZoomUi/ZoomUI.java
@@ -12,35 +12,40 @@ import de.ft.interitus.UI.UIElements.FormattingFrame;
 import de.ft.interitus.UI.UIElements.Grid;
 import de.ft.interitus.UI.UIElements.PlaceHolder;
 import de.ft.interitus.UI.UIElements.UIElements.Button;
+import de.ft.interitus.UI.UIElements.UIElements.TabBar.Tab;
 import de.ft.interitus.UI.UIElements.UIElements.labels.TextLabel;
 import de.ft.interitus.UI.UIVar;
 import de.ft.interitus.UI.Viewport;
 import de.ft.interitus.UI.uiManagement.UI;
+import de.ft.interitus.Var;
+import de.ft.interitus.events.EventVar;
 import de.ft.interitus.events.UI.UIZoomEvent;
+import de.ft.interitus.events.global.GlobalEventAdapter;
+import de.ft.interitus.events.global.GlobalTabClickEvent;
 import de.ft.interitus.loading.AssetLoader;
 
 public class ZoomUI extends UI {
-    private final Grid grid=new Grid(0,0,0,0);
-    private final TextLabel textLabel=new TextLabel("TextLabel");
-   private final Button plus=new Button();
-   private final Button minus=new Button();
-   private final Button reset =new Button();
+    private final Grid grid = new Grid(0, 0, 0, 0);
+    private final TextLabel textLabel = new TextLabel("", 50);
+    private final Button plus = new Button();
+    private final Button minus = new Button();
+    private final Button reset = new Button();
 
-    private final FormattingFrame formattingFrame=new FormattingFrame(grid);
+    private final FormattingFrame formattingFrame = new FormattingFrame(grid);
 
-    public ZoomUI(){
+    public ZoomUI() {
         plus.setImage(AssetLoader.PlusButton);
-        plus.setBounds(0,0,16,16);
+        plus.setBounds(0, 0, 16, 16);
         plus.setWidthoverText(true);
         plus.setHeightoverText(true);
 
         minus.setImage(AssetLoader.MinusButton);
-        minus.setBounds(0,0,16,16);
+        minus.setBounds(0, 0, 16, 16);
         minus.setWidthoverText(true);
         minus.setHeightoverText(true);
 
         reset.setImage(AssetLoader.ResetButton);
-        reset.setBounds(0,0,80,30);
+        reset.setBounds(0, 0, 80, 30);
         reset.setWidthoverText(true);
         reset.setHeightoverText(true);
 
@@ -58,27 +63,31 @@ public class ZoomUI extends UI {
 
 
         Viewport.zoomEvent.addListener(new UIZoomEvent() {
-             @Override
-           public void zoomStart() {
-            ZoomUI.super.enabled = true;
+            @Override
+            public void zoomStart() {
+                ZoomUI.super.enabled = true;
             }
         });
 
         formattingFrame.setBorderThickness(2);
         formattingFrame.setBorderRadius(5);
-        formattingFrame.setFillColor(new Color(41f/255f,42f/255,45f/255f,1f));
-        formattingFrame.setBorderColor(new Color(112f/255f,112f/255,112f/255f,1f));
+        formattingFrame.setFillColor(new Color(41f / 255f, 42f / 255, 45f / 255f, 1f));
+        formattingFrame.setBorderColor(new Color(112f / 255f, 112f / 255, 112f / 255f, 1f));
     }
 
     @Override
     protected void draw() {
-        textLabel.setText(Viewport.getZoomPercentage()+"%");
-        if(plus.isjustPressednormal())  Viewport.increaseZoom(ProgramingSpace.cam);
-        if(minus.isjustPressednormal()) Viewport.decreaseZoom(ProgramingSpace.cam);
-        if(reset.isjustPressednormal()) Viewport.resetZoom(ProgramingSpace.cam);
+        textLabel.setText(Viewport.getZoomPercentage() + "%");
+        if (plus.isjustPressednormal()) Viewport.increaseZoom(ProgramingSpace.cam);
+        if (minus.isjustPressednormal()) Viewport.decreaseZoom(ProgramingSpace.cam);
+        if (reset.isjustPressednormal()) Viewport.resetZoom(ProgramingSpace.cam);
 
-        formattingFrame.setPosition(Gdx.graphics.getWidth()-UIVar.abstandvonRand-formattingFrame.w-5, UIVar.programmflaeche_y+UIVar.programmflaeche_h-formattingFrame.h-5);
+        formattingFrame.setPosition(Gdx.graphics.getWidth() - UIVar.abstandvonRand - formattingFrame.w - 5, UIVar.programmflaeche_y + UIVar.programmflaeche_h - formattingFrame.h - 5);
         formattingFrame.draw();
+
+        if(Gdx.input.isButtonJustPressed(0)) {
+            super.enabled = false;
+        }
 
 
     }
--- core/src/de/ft/interitus/UI/UIElements/UIElements/labels/TextLabel.java
+++ core/src/de/ft/interitus/UI/UIElements/UIElements/labels/TextLabel.java
@@ -11,11 +11,20 @@ import de.ft.interitus.UI.UIElements.UIElements.UIElement;
 import de.ft.interitus.WindowManager;
 
 public class TextLabel extends UIElement {
-    String text="";
-    GlyphLayout glyphLayout=new GlyphLayout();
-
+    private String text="";
+    private final GlyphLayout glyphLayout=new GlyphLayout();
+    private boolean hasFixedWidth = false;
     public TextLabel(){
 
+    }
+
+    /**
+     * Call this constructor if you want to have a fixed size text label
+     */
+    public TextLabel(String text,int w) {
+        this.hasFixedWidth = true;
+        this.text = text;
+        this.w = w;
     }
     public TextLabel(String text){
         this.text=text;
@@ -30,7 +39,8 @@ public class TextLabel extends UIElement {
     public void draw() {
         glyphLayout.setText(WindowManager.font, this.text);
         UI.UIbatch.begin();
-        w=(int)(glyphLayout.width+ margin *2);
+        if(!hasFixedWidth)
+            w=(int)(glyphLayout.width+ margin *2);
         h=(int)(glyphLayout.height+ margin *2);
 
         WindowManager.font.draw(UI.UIbatch, glyphLayout, this.x + this.w / 2f - glyphLayout.width / 2 + margin, y + this.h / 2f + glyphLayout.height/2);
