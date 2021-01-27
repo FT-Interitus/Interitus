6e9761d153d3e14ca8e6250b330c3a9d71d822e0
Coder246 <tim-arnold@outlook.de>
2021-01-27 16:04:03 +0100

    Added Drag

--- Interitus Electron/src/renderer.ts
+++ Interitus Electron/src/renderer.ts
@@ -4,52 +4,73 @@
 // nodeIntegration is set to true in webPreferences.
 // Use preload.js to selectively enable features
 import * as $ from "jquery"
+
+
 function initTabBarController() {
+    var projectcounter = 0;
+    const project_tabs = document.getElementById("project-tabs");
+    const project_tab_container = document.querySelector(".tab-container");
 
-    var projectcounter =0;
-  const project_tabs =  document.getElementById("project-tabs");
+    function handleDragStart(e:any) {
+        this.style.cursor = "move";
+        this.style.opacity = '0.4';
+    }
 
+    function handleDragEnd(e:any) {
+        this.style.cursor = "pointer";
+        this.style.opacity = '1';
+    }
 
-
-        $(document).on('click', '.projectTabContainer a',function(e) {
-            const currentAttrValue = $(this).attr('href');
-            if(!$(this).hasClass("project-close")) {
+    $(document).on('click', '.projectTabContainer', function (e) {
+        const currentAttrValue = $(this).children("a,p").attr('href');
+        if (!$(this).hasClass("project-close")) {
 
             // Show/Hide Tabs
-            if(!$(this).hasClass("cnp-button")) {
+            if (!$(this).children("a,p").hasClass("cnp-button")) {
                 $('.tabs ' + currentAttrValue).show().siblings().hide();
 
                 $('li.active').removeClass('active');
 
                 // Change/remove current tab to active
-                $(this).parent().parent('li').addClass('active');
+                $(this).parent('li').addClass('active');
 
-            }else {
+            } else {
                 projectcounter++;
                 const newtab = document.createElement("li")
                 const projectTabContainer = document.createElement("div");
                 projectTabContainer.className = "projectTabContainer";
-                const atag = document.createElement("a");
-                atag.href = "Projekt " + projectcounter
-                atag.innerHTML = "Neues Projekt " + projectcounter;
-                newtab.insertAdjacentElement('beforeend', atag)
+                projectTabContainer.draggable = true;
+                projectTabContainer.addEventListener('dragstart', handleDragStart, false);
+                projectTabContainer.addEventListener('dragend', handleDragEnd, false);
+                const ptag = document.createElement("p");
+                ptag.innerHTML = "Neues Projekt " + projectcounter;
+                ptag.style.display = "inline";
+                newtab.insertAdjacentElement('beforeend', ptag)
                 const closetag = document.createElement("a");
-                closetag.href = "#";
-                closetag.innerText = "✖";
+                closetag.innerText = "x";
                 closetag.className = "project-close";
-                projectTabContainer.insertAdjacentElement('beforeend', atag);
+                closetag.style.display = "inline";
+                projectTabContainer.insertAdjacentElement('beforeend', ptag);
                 projectTabContainer.insertAdjacentElement('beforeend', closetag);
                 newtab.insertAdjacentElement('beforeend', projectTabContainer);
                 $('#project-tabs').append(newtab);
 
 
             }
-            }else{
+        } else {
 
-                $(this).parent().parent().remove();
+            if ($(this).parent().hasClass("active")) {
+                console.log("active state")
+                $('#project-home-link').addClass("active");
             }
-            e.preventDefault();
-        });
+
+            $(this).parent().remove();
+        }
+        e.preventDefault();
+    });
+        
+
+
 
 
 }
